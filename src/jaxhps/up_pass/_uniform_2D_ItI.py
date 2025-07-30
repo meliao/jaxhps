Removed incoming part impedance computation and fixed accuracy tests.

src/jaxhps/up_pass/_uniform_2D_ItI.py
@@ -89,7 +89,6 @@ def up_pass_uniform_2D_ItI(
     # Get leaf-level h_in array, which is an einsum between the particular soln and
     # the QH array stored in the PDEProblem.
     h_in = jnp.einsum("ij,kjl->kil", pde_problem.QH, v)
-    g_in = jnp.einsum("ij,kjl->kil", pde_problem.QG, v)
 
     logging.debug(
         "up_pass_uniform_2D_ItI: after local solve, h_in shape = %s",
@@ -103,13 +102,10 @@ def up_pass_uniform_2D_ItI(
 
         nnodes, nbdry, nsrc = h_in.shape
         h_in = h_in.reshape(nnodes // 4, 4, nbdry, nsrc)
-        g_in = g_in.reshape(nnodes // 4, 4, nbdry, nsrc)
         D_inv = D_inv_lst[i]
         BD_inv = BD_inv_lst[i]
 
-        h_in, g_in, g_tilde, h_ext_child = vmapped_assemble_boundary_data(
-            h_in, g_in, D_inv, BD_inv
-        )
+        h_in, g_tilde = vmapped_assemble_boundary_data(h_in, D_inv, BD_inv)
         g_tilde_lst.append(g_tilde)
 
     logging.debug(
@@ -125,7 +121,6 @@ def up_pass_uniform_2D_ItI(
             jnp.squeeze(g_tilde, axis=-1) for g_tilde in g_tilde_lst
         ]
         h_in = jnp.squeeze(h_in, axis=-1)
-        g_in = jnp.squeeze(g_in, axis=-1)
         # h_ext_child = jnp.squeeze(h_ext_child, axis=-1)
         logging.debug(
             "up_pass_uniform_2D_ItI: it's not multi source so squeezing h_in to shape %s",
@@ -139,7 +134,6 @@ def up_pass_uniform_2D_ItI(
             v,
             g_tilde_lst,
             jnp.squeeze(h_in, axis=0),
-            jnp.squeeze(g_in, axis=0),
         )
 
     return out
@@ -148,7 +142,6 @@ def up_pass_uniform_2D_ItI(
 @jax.jit
 def assemble_boundary_data(
     h_in: jax.Array,
-    g_in: jax.Array,
     D_inv: jax.Array,
     BD_inv: jax.Array,
 ) -> Tuple[jax.Array, jax.Array]:
@@ -157,15 +150,12 @@ def assemble_boundary_data(
 
     Args:
         h_in (jax.Array): Has shape (4, 4 * nside, n_src) where nside is the number of discretization points along each side of the nodes being merged.
-        g_in (jax.Array): Has shape (4, 4 * nside, n_src) where nside is the number of discretization points along each side of the nodes being merged.
         D_inv (jax.Array): Has shape (8 * nside, 8 * nside)
         BD_inv (jax.Array): Has shape (8 * nside, 8 * nside)
 
     Returns:
         h : jax.Array
             Has shape (8 * nside, n_src) and is the outgoing impedance data due to the particular solution on the merged node.
-        g : jax.Array
-            Has shape (8 * nside, n_src) and is the incoming impedance data due to the particular solution on the merged nodes.
         g_tilde : jax.Array
             Has shape (8 * nside, n_src) and is the incoming impedance data due to the particular solution on the merged node, evaluated along the merge interfaces.
     """
@@ -199,28 +189,6 @@ def assemble_boundary_data(
 
     h_ext_child = jnp.concatenate([h_a_1, h_b_2, h_c_3, h_d_4])
 
-    # Break up the g array in the same way to form g_int_child and g_ext_child
-    g_a = g_in[0]
-    g_a_1 = jnp.concatenate([g_a[-nside:], g_a[:nside]])
-    g_a_5 = g_a[nside : 2 * nside]
-    g_a_8 = jnp.flipud(g_a[2 * nside : 3 * nside])
-    g_b = g_in[1]
-    g_b_2 = g_b[: 2 * nside]
-    g_b_6 = g_b[2 * nside : 3 * nside]
-    g_b_5 = jnp.flipud(g_b[3 * nside : 4 * nside])
-    g_c = g_in[2]
-    g_c_6 = jnp.flipud(g_c[:nside])
-    g_c_3 = g_c[nside : 3 * nside]
-    g_c_7 = g_c[3 * nside : 4 * nside]
-    g_d = g_in[3]
-    g_d_8 = g_d[:nside]
-    g_d_7 = jnp.flipud(g_d[nside : 2 * nside])
-    g_d_4 = g_d[2 * nside : 4 * nside]
-    g_int_child = jnp.concatenate(
-        [g_b_5, g_d_8, g_b_6, g_d_7, g_a_5, g_c_6, g_c_7, g_a_8]
-    )
-    g_ext_child = jnp.concatenate([g_a_1, g_b_2, g_c_3, g_d_4])
-
     g_tilde = -1 * D_inv @ h_int_child
 
     # g_tilde is ordered like a_5, a_8, c_6, c_7, b_5, b_6, d_7, d_8.
@@ -242,16 +210,13 @@ def assemble_boundary_data(
 
     h = h_ext_child - BD_inv @ h_int_child
 
-    g = g_ext_child - BD_inv @ g_int_child
-
     # h is ordered like h_a_1, h_b_2, h_c_3, h_d_4. Need to
     # roll it so that it's ordered like [bottom, left, right, top].
     h = jnp.roll(h, -nside, axis=0)
-    g = jnp.roll(g, -nside, axis=0)
 
-    return (h, g, g_tilde, h_ext_child)
+    return (h, g_tilde)
 
 
 vmapped_assemble_boundary_data = jax.vmap(
-    assemble_boundary_data, in_axes=(0, 0, 0, 0), out_axes=(0, 0, 0, 0)
+    assemble_boundary_data, in_axes=(0, 0, 0), out_axes=(0, 0)
 )

tests/test_accuracy/cases.py
@@ -326,17 +326,17 @@ def source_4(x: jax.Array) -> jax.Array:
     # f(x,y) = (4 \\kappa^2 x^2 + 4 \\kappa^2 y^2 + 2 \\kappa) v(x,y)
     coeff = (
         4 * KAPPA**2 * (jnp.square(x[..., 0]) + jnp.square(x[..., 1]))
-        + 2 * KAPPA
+        + 4 * KAPPA
     )
     return coeff * v_4(x)
 
 
 def dudx_4(x: jax.Array) -> jax.Array:
-    return dwdx_4(x) + dvdx_4(x)
+    return dvdx_4(x) + dwdx_4(x)
 
 
 def dudy_4(x: jax.Array) -> jax.Array:
-    return dwdy_4(x) + dvdy_4(x)
+    return dvdy_4(x) + dwdy_4(x)
 
 
 TEST_CASE_ITI_PART_HOMOG = {
@@ -348,4 +348,7 @@ TEST_CASE_ITI_PART_HOMOG = {
     K_SOLN: u_4,
     K_DUDX: dudx_4,
     K_DUDY: dudy_4,
+    K_PART_SOLN_DUDX: dvdx_4,
+    K_PART_SOLN_DUDY: dvdy_4,
+    K_PART_SOLN: v_4,
 }

tests/test_accuracy/test_ItI_up_pass_accuracy.py
@@ -2,8 +2,6 @@ import logging
 from typing import Dict
 import jax.numpy as jnp
 import jax
-import matplotlib.pyplot as plt
-import pytest
 
 from jaxhps._discretization_tree import DiscretizationNode2D
 from jaxhps._domain import Domain
@@ -21,14 +19,14 @@ from .cases import (
     YMIN,
     YMAX,
     ETA,
-    TEST_CASE_HELMHOLTZ_ITI_COMPLEX_COEFFS,
+    TEST_CASE_ITI_PART_HOMOG,
     K_XX_COEFF,
     K_YY_COEFF,
     K_SOURCE,
-    K_SOLN,
-    K_DUDX,
-    K_DUDY,
     K_I_COEFF,
+    K_PART_SOLN_DUDY,
+    K_PART_SOLN_DUDX,
+    K_PART_SOLN,
 )
 
 ATOL_NONPOLY = 1e-8
@@ -75,51 +73,46 @@ def check_up_pass_accuracy_2D_ItI_uniform_Helmholtz_like(
     # Do an up pass to compute the incoming and outgoing impedance data
     source = test_case[K_SOURCE](domain.interior_points)
 
-    _, _, h_last, g_last = up_pass_uniform_2D_ItI(
+    _, _, h_last = up_pass_uniform_2D_ItI(
         source=source, pde_problem=pde_problem, return_bdry_imp_data=True
     )
 
     # Assemble incoming impedance data
     q = domain.boundary_points.shape[0] // 4
-    boundary_u = test_case[K_SOLN](domain.boundary_points)
+    boundary_u = test_case[K_PART_SOLN](domain.boundary_points)
     boundary_u_normals = jnp.concatenate(
         [
-            -1 * test_case[K_DUDY](domain.boundary_points[:q]),
-            test_case[K_DUDX](domain.boundary_points[q : 2 * q]),
-            test_case[K_DUDY](domain.boundary_points[2 * q : 3 * q]),
-            -1 * test_case[K_DUDX](domain.boundary_points[3 * q :]),
+            -1 * test_case[K_PART_SOLN_DUDY](domain.boundary_points[:q]),
+            test_case[K_PART_SOLN_DUDX](domain.boundary_points[q : 2 * q]),
+            test_case[K_PART_SOLN_DUDY](domain.boundary_points[2 * q : 3 * q]),
+            -1 * test_case[K_PART_SOLN_DUDX](domain.boundary_points[3 * q :]),
         ]
     )
-    incoming_imp_data = boundary_u_normals + 1j * pde_problem.eta * boundary_u
     outgoing_imp_data = boundary_u_normals - 1j * pde_problem.eta * boundary_u
 
-    # Plot outgoing_imp_data against h_last
-    plt.plot(outgoing_imp_data.real, label="Outgoing Impedance Data.real")
-    plt.plot(outgoing_imp_data.imag, label="Outgoing Impedance Data.imag")
-    # plt.plot(incoming_imp_data.real, label="Incoming Impedance Data.real")
-    # plt.plot(incoming_imp_data.imag, label="Incoming Impedance Data.imag")
-    plt.plot(h_last.real, label="h_last.real")
-    plt.plot(h_last.imag, label="h_last.imag")
-    # plt.plot(g_last.real, label="g_last.real")
-    # plt.plot(g_last.imag, label="g_last.imag")
-    plt.legend()
-    plt.show()
+    # Plot the magnitudes
+    # plt.plot(jnp.abs(h_last), ".-", label="computed out")
+    # plt.plot(jnp.abs(outgoing_imp_data), "x-", label="expected out")
+    # # plt.plot(jnp.abs(incoming_imp_data), label="expected in")
+    # plt.legend()
+    # plt.show()
 
     # Check the outgoing impedance data
-    assert jnp.allclose(h_last, outgoing_imp_data, atol=ATOL, rtol=RTOL)
+    assert jnp.allclose(
+        h_last, outgoing_imp_data, atol=ATOL_NONPOLY, rtol=RTOL
+    )
 
     # Check the incoming impedance data
-    assert jnp.allclose(g_last, incoming_imp_data, atol=ATOL, rtol=RTOL)
+    # assert jnp.allclose(g_last, incoming_imp_data, atol=ATOL, rtol=RTOL)
 
 
 class Test_accuracy_up_pass_2D_ItI_uniform:
-    @pytest.mark.skip
     def test_0(self, caplog) -> None:
         """Testing whether the computed outgoing and incoming particular soln impedance data matches against expectatiosn after 1 merge level."""
         caplog.set_level(logging.DEBUG)
 
         check_up_pass_accuracy_2D_ItI_uniform_Helmholtz_like(
-            DOMAIN_ITI_NONPOLY, TEST_CASE_HELMHOLTZ_ITI_COMPLEX_COEFFS
+            DOMAIN_ITI_NONPOLY, TEST_CASE_ITI_PART_HOMOG
         )
         jax.clear_caches()
 
@@ -127,9 +120,9 @@ class Test_accuracy_up_pass_2D_ItI_uniform:
         """Testing whether the computed outgoing and incoming particular soln impedance data matches against expectatiosn after the local solve."""
         caplog.set_level(logging.DEBUG)
 
-        domain = Domain(p=P, q=Q, root=ROOT_ITI, L=0)
+        domain = Domain(p=P_NONPOLY, q=Q_NONPOLY, root=ROOT_ITI, L=0)
 
-        test_case = TEST_CASE_HELMHOLTZ_ITI_COMPLEX_COEFFS
+        test_case = TEST_CASE_ITI_PART_HOMOG
         d_xx_coeffs = test_case[K_XX_COEFF](domain.interior_points)
         d_yy_coeffs = test_case[K_YY_COEFF](domain.interior_points)
         i_coeffs = test_case[K_I_COEFF](domain.interior_points)
@@ -168,23 +161,26 @@ class Test_accuracy_up_pass_2D_ItI_uniform:
 
         v = v.squeeze()
 
-        outgoing_imp = pde_problem.QH @ v
-        incoming_imp = pde_problem.QG @ v
+        outgoing_imp_part = pde_problem.QH @ v
+        incoming_imp_part = pde_problem.QG @ v
 
         # Construct the expected impedance data
         q = pde_problem.domain.boundary_points.shape[0] // 4
-        boundary_u = test_case[K_SOLN](pde_problem.domain.boundary_points)
+        boundary_u = test_case[K_PART_SOLN](pde_problem.domain.boundary_points)
         boundary_u_normals = jnp.concatenate(
             [
-                -1 * test_case[K_DUDY](pde_problem.domain.boundary_points[:q]),
-                test_case[K_DUDX](
+                -1
+                * test_case[K_PART_SOLN_DUDY](
+                    pde_problem.domain.boundary_points[:q]
+                ),
+                test_case[K_PART_SOLN_DUDX](
                     pde_problem.domain.boundary_points[q : 2 * q]
                 ),
-                test_case[K_DUDY](
+                test_case[K_PART_SOLN_DUDY](
                     pde_problem.domain.boundary_points[2 * q : 3 * q]
                 ),
                 -1
-                * test_case[K_DUDX](
+                * test_case[K_PART_SOLN_DUDX](
                     pde_problem.domain.boundary_points[3 * q :]
                 ),
             ]
@@ -194,21 +190,19 @@ class Test_accuracy_up_pass_2D_ItI_uniform:
         expected_in = boundary_u_normals + 1j * pde_problem.eta * boundary_u
 
         # Plot the outgoing data
-        plt.plot(outgoing_imp.real, ".-", label="computed out.real")
-        plt.plot(outgoing_imp.imag, label="computed out.imag")
-        plt.plot(incoming_imp.real, "x-", label="computed in.real")
-        plt.plot(incoming_imp.imag, label="computed in.imag")
-        # plt.plot(expected_out.real, label="Expected Outgoing Data.real")
-        # plt.plot(expected_out.imag, label="Expected Outgoing Data.imag")
-        plt.legend()
-        plt.show()
+        # plt.plot(jnp.abs(outgoing_imp_part), ".-", label="computed out")
+        # plt.plot(jnp.abs(incoming_imp_part), "x-", label="computed in")
+        # plt.plot(jnp.abs(expected_out), label="expected out")
+        # plt.plot(jnp.abs(expected_in), label="expected in")
+        # plt.legend()
+        # plt.show()
 
         # Check equality
         assert jnp.allclose(
-            outgoing_imp, expected_out, atol=ATOL_NONPOLY, rtol=RTOL
+            outgoing_imp_part, expected_out, atol=ATOL_NONPOLY, rtol=RTOL
         )
         assert jnp.allclose(
-            incoming_imp, expected_in, atol=ATOL_NONPOLY, rtol=RTOL
+            incoming_imp_part, expected_in, atol=ATOL_NONPOLY, rtol=RTOL
         )
 
         jax.clear_caches()

tests/test_accuracy/test_nosource_accuracy.py
@@ -41,7 +41,6 @@ from .cases import (
 
 # Imports necessary for the plotting functions, which are currently disabled.
 # These are useful to sniff out bugs.
-from jaxhps._utils import plot_soln_from_cheby_nodes
 # import matplotlib.pyplot as plt
 
 ATOL_NONPOLY = 1e-8
@@ -147,12 +146,12 @@ def check_merge_accuracy_nosource_2D_ItI_uniform_Helmholtz_like(
     )
 
     # Plot the solution. This function can be found in src//_utils.py
-    plot_soln_from_cheby_nodes(
-        cheby_nodes=domain.interior_points.reshape(-1, 2),
-        corners=None,
-        expected_soln=expected_soln.real.flatten(),
-        computed_soln=computed_soln.real.flatten(),
-    )
+    # plot_soln_from_cheby_nodes(
+    #     cheby_nodes=domain.interior_points.reshape(-1, 2),
+    #     corners=None,
+    #     expected_soln=expected_soln.real.flatten(),
+    #     computed_soln=computed_soln.real.flatten(),
+    # )
 
     max_diff = jnp.max(jnp.abs(computed_soln - expected_soln))
 
@@ -402,7 +401,7 @@ class Test_accuracy_2D_ItI_uniform:
         """Tests to make sure things run without error."""
         caplog.set_level(logging.DEBUG)
         check_merge_accuracy_nosource_2D_ItI_uniform_Helmholtz_like(
-            DOMAIN_ITI, TEST_CASE_ITI_PART_HOMOG
+            DOMAIN_ITI_NONPOLY, TEST_CASE_ITI_PART_HOMOG
         )
 
 